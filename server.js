f9581e8d7aee8969c195770f2fa1eb4b460f674a
Lin Liu <liulin1987315@gmail.com>
2016-04-20 21:41:37 +0800

    use express to start server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
+
 //==============================================
 
 //1. Now add the middleware into your server:
@@ -24,10 +25,21 @@ app.use(require('webpack-hot-middleware')(compiler));
 //================================================
 
 
+
+//1. what is __dirname
+//The name of the directory that the currently executing script resides in.
+//Example: running node example.js from /Users/mjr
+//console.log(__dirname); The result is "/Users/mjr"
+
+//2. what is path.join
+//var filePath = path.join('foo', 'bar)';
+// 'foo/bar' on OSX and Linux
+// 'foo\\bar' on Windows
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'examples/index.html'));
 });
 
+//The app starts a server and listens on port 3000 for connections. 
 app.listen(port, '0.0.0.0', function(err) {
   if (err) {
     console.log(err);
