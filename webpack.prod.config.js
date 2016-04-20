62ecce8fcfa2c202263a4786e37219b1bfe97e8b
Lin Liu <liulin1987315@gmail.com>
2016-04-20 21:28:11 +0800

    add annotation for the webpack production config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
+
+
+//It moves every require("style.css") in entry chunks into a separate css output file. 
+//So your styles are no longer inlined into the javascript, 
+//but separate in a css bundle file (styles.css). 
+//If your total stylesheet volume is big, 
+//it will be faster because the stylesheet bundle is loaded in parallel to the javascript bundle.
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
@@ -7,6 +14,14 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
+
+    //"var" - Export by setting a variable: var Library = xxx (default)
+    //"this" - Export by setting a property of this: this["Library"] = xxx
+    //"commonjs" - Export by setting a property of exports: exports["Library"] = xxx
+    //"commonjs2" - Export by setting module.exports: module.exports = xxx
+    //"amd" - Export to AMD (optionally named - set the name via the library option)
+    //"umd" - Export to AMD, CommonJS2 or as property in root
+    //Default: "var"
     libraryTarget: "commonjs2"
   },
   externals: {
@@ -17,8 +32,19 @@ module.exports = {
         "NODE_ENV": JSON.stringify("production")
       }
     }),
+
+    //Search for equal or similar files and deduplicate them in the output. 
+    //This comes with some overhead for the entry chunk, but can reduce file size effectively.
+    //Note: Don’t use it in watch mode. Only for production builds.
     new webpack.optimize.DedupePlugin(),
+
+    //Minimize all JavaScript output of chunks. 
+    //Loaders are switched into minimizing mode. 
     new webpack.optimize.UglifyJsPlugin(),
+
+    //new ExtractTextPlugin([id: string], filename: string, [options])
+    //filename => the filename of the result file
+    //allChunks extract from all additional chunks too (by default it extracts only from the initial chunk(s))
     new ExtractTextPlugin("bundle.css", { allChunks: true })
   ],
   module: {
@@ -28,6 +54,9 @@ module.exports = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: 'url?limit=10000'
       },
+
+      //ExtractTextPlugin.extract([notExtractLoader], loader, [options])
+      //loader the loader(s) that should be used for converting the resource to a css exporting module.
       { test: /\.(s)*css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader") }
     ]
   }
